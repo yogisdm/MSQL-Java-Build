9a8575d5379b8dd590dd36c3fb2deb4a0eebe9f9
Shivayogi Kamat <yogisdmec007@gmail.com>
2019-05-17 13:59:20 +0530

    db and table creation

--- createDB.sh
+++ createDB.sh
@@ -1,3 +1,9 @@
-echo "Creating Wordpress DB Users and grating privileges with already collected information...\n"
+echo "Creating Database jwt and tables.....\n"
 
-sudo mysqladmin -u root -pPassword@123 create blog
+sudo mysqladmin -u root -pPassword@123 create jwt
+
+mysql -u root -D mysql -pPassword@123 -e "use mysql;UPDATE user SET Host='%' WHERE Host='localhost' AND User='root';"
+
+mysql -u root -pPassword@123 -e "use jwt;CREATE table USER (first_name, last_name, email, username, password, regdate);"
+
+sudo systemctl restart mysql
