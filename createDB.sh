027d5a2ccb361d9b569f96caf9d595b08c0b94b7
Shivayogi Kamat <yogisdmec007@gmail.com>
2019-05-17 14:44:45 +0530

    corrected mysql syntax error

--- createDB.sh
+++ createDB.sh
@@ -4,6 +4,7 @@ sudo mysqladmin -u root -pPassword@123 create jwt
 
 mysql -u root -D mysql -pPassword@123 -e "use mysql;UPDATE user SET Host='%' WHERE Host='localhost' AND User='root';"
 
-mysql -u root -pPassword@123 -e "use jwt;CREATE table USER (first_name, last_name, email, username, password, regdate);"
+mysql -u root -pPassword@123 -e "use jwt;CREATE table USER (first_name varchar(20), last_name varchar(20), email varchar(20), username varchar(20), password varchar(20), regdate varchar(20));"
 
 sudo systemctl restart mysql
+
